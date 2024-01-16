7be2164dc80b0d2cf2a750d726d1eb9b67c52afa
moriken4320 <moriken.4320@gmail.com>
2024-01-16 15:12:57 +0900

    first commit

---
+++ restApiOnSpring/src/main/kotlin/com/example/restapionspring/RestApiOnSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.restapionspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiOnSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApiOnSpringApplication>(*args)
+}
---
+++ restApiOnSpring/src/test/kotlin/com/example/restapionspring/RestApiOnSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.restapionspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiOnSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
